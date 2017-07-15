0c0d2e224f021f775bb855a06393773c32b341a4
Mircea Eliade <roszcz@gmail.com>
2017-07-16 00:50:49 +0200

    Favor the 'valid' convolution mode

--- models/abstract.py
+++ models/abstract.py
@@ -358,31 +358,37 @@ class FirstTry(AbstractNet):
             c1 = tf.expand_dims(self.putin, -1)
             c1 = self.conv(c1,
                            filters = 32,
-                           width = 32,
-                           stride = 2)
+                           width = 128,
+                           stride = 1)
 
         with tf.name_scope('conv2'):
             c2 = self.conv(c1,
                            filters = 64,
-                           width = 48,
-                           stride = 2)
+                           width = 128,
+                           stride = 1)
 
         with tf.name_scope('conv3'):
             c3 = self.conv(c2,
-                           filters = 72,
+                           filters = 64,
                            width = 32,
-                           stride = 3)
+                           stride = 1)
+
+        with tf.name_scope('conv4'):
+            c4 = self.conv(c3,
+                           filters = 64,
+                           width = 10,
+                           stride = 1)
 
         print 'Shrinked:', c3.get_shape().as_list()
 
         # Stretch up (with transposed convolutions)
         with tf.name_scope('deconv2'):
-            r1 = tf.reshape(c3, [self.batch_size, -1])
+            r1 = tf.reshape(c4, [self.batch_size, -1])
             r1 = tf.nn.softmax(r1)
             r1 = tf.expand_dims(r1, -1)
             r1 = self.conv(r1,
                            filters = 1,
-                           width = 61,
+                           width = 85,
                            stride = 1)
 
         # with tf.name_scope('deconv1'):
